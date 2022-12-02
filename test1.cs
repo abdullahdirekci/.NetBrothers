9335247efca8e757bb1e21d101a2fa842d619694
ixmb <beyogluinc@gmail.com>
2022-12-02 19:37:02 +0300

    test

--- test1.cs
+++ test1.cs
@@ -1,8 +1,20 @@
 
-public void SayHello(string who){
-    
+public class Hello{
+    public string SayHello(string who){
+        string cumlecik;
+        if (who != "")
+        {
+            cumlecik = "Hi" + who;
+        }
+        else
+        {
+            cumlecik = "Hello world";
+        }
+        return cumlecik;
+    }
 }
 
 public static void main(string[] args){
-
+    Hello hi = new Hello();
+    hi.SayHello("Mert");
 }
